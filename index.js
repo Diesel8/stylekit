8ab7865588f77ed6d5ee36fe88cc9f9cb9b0bacf
Ken Andries <ken.andries.1992@gmail.com>
2018-03-29 20:29:04 +0200

    v0.3.2

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 let mix = require('laravel-mix');
 
-class Terminal2 {
-  dependencies() {
-    return ["laravel-mix-svg", "laravel-mix-tailwind", "laravel-mix-purgecss"];
-  }
+require('laravel-mix-svg');
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
-  boot() {
-    Mix.js('resources/assets/js/app.js', 'public/js')
-      .copy('node_modules/terminal2-stylekit/fonts', 'public/fonts')
-      .sass('resources/assets/sass/app.scss', 'public/css')
-      .tailwind('node_modules/terminal2-stylekit/tailwind.js')
-      .svg({
-        assets: [
-          './resources/assets/svg/',
-          'node_modules/terminal2-stylekit/svg',
-        ]
-      })
-      .purgeCss()
-      .version()
-    ;
-  }
+module.exports = () => {
+  return mix.js('resources/assets/js/app.js', 'public/js')
+    .copy('node_modules/terminal2-stylekit/fonts', 'public/fonts')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .tailwind('node_modules/terminal2-stylekit/tailwind.js')
+    .svg({
+      assets: [
+        'resources/assets/svg/',
+        'node_modules/terminal2-stylekit/svg/',
+      ]
+    })
+    .purgeCss()
+    .version()
+  ;
 }
-
-mix.extend('terminal2', new Terminal2());
-
